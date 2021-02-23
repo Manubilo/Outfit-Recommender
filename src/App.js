2b4dad98caffd27f1390a91e9ac3282b5fbc3c1e
Manubilo <mjdelgado@pucp.pe>
2021-02-23 16:10:27 -0500

    added sample navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Garments from './components/pages/garments/Garments';
 import Moods from './components/pages/moods/Moods';
 import Outfits from './components/pages/outfits/Outfits';
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/garments" component={Garments}></Route>
-        <Route exact path="/moods" component={Moods}></Route>
-        <Route exact path="/outfits" component={Outfits}></Route>
-      </Switch>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path="/garments" component={Garments}></Route>
+          <Route exact path="/moods" component={Moods}></Route>
+          <Route exact path="/outfits" component={Outfits}></Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>im a navbar</h1>
+    <div className="flex h-20 justify-around items-center bg-red-400 text-white">
+      {/* Logo and Website name */}
+      <div>
+        <h1 className="text-lg">Outfit Recommender</h1>
+      </div>
+      {/* Links */}
+      <div>
+        <Link className="mx-10" to="/garments">
+          Garments
+        </Link>
+        <Link className="mx-10" to="/moods">
+          Moods
+        </Link>
+        <Link className="mx-10" to="/outfits">
+          Outfits
+        </Link>
+      </div>
     </div>
   );
 };
